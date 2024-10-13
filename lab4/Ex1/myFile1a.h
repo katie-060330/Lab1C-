1b59e0dafdaaa0138797a2129a0f3ba9ad1353ef
katie-060330 <kate.lonsway@outlook.com>
2024-10-13 11:24:03 -0400

    ading lab4 mods

---
+++ lab4/Ex1/myFile1a.h
@@ -0,0 +1,16 @@
+/*myFile1a, Ex1 a4 CSI2372A*/
+
+#include <iostream>
+using namespace std;
+
+class Course {
+    int num;
+    int hours;
+public:
+    Course(int, int); 	//constructor
+    int getNum();
+    int getHours();
+};
+
+
+
