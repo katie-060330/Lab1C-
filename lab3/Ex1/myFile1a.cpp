bf8791eee7c9338103f11d403cc117cc4abf9b97
katie-060330 <kate.lonsway@outlook.com>
2024-10-13 11:20:59 -0400

    adding lab 3

---
+++ lab3/Ex1/myFile1a.cpp
@@ -0,0 +1,18 @@
+/*Ex1a - a3 CSI2372A*/
+
+#include <iostream>
+using namespace std;
+
+enum Color { club, diamond, spades, heart };
+enum Face { seven, eight, nine, ten, jack, queen, king, ace };
+
+struct Card{
+	Color color;
+	Face face;
+};
+
+int main(){
+	Card game[32];
+	//YOUR CODE HERE
+}
+
