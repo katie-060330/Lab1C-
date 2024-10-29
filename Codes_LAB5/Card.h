efb7a1225ebd58f479a8a9e761eeb77258382909
mahreenabbasi <mahreenabbasi00@gmail.com>
2024-10-29 14:41:05 -0400

    player

--- Codes_LAB5/Card.h
+++ Codes_LAB5/Card.h
@@ -4,6 +4,7 @@ using namespace std;
 
 enum color { club, diamond, heart, spade };
 
+
 class Card {
 public:
     Card(color c = club, int v = 1);
@@ -22,6 +23,29 @@ Card::Card(color c, int v) {
     val = v;
 }
 
+
+
 void Card::write(){
-    cout<<val<< " " <<col<<endl;
+    color suit;
+    switch(suit ){
+        case club: 
+        std::cout<<"club";break;
+        case diamond:
+        std::cout <<"diamond";break;
+        case heart: 
+        std::cout <<"heart";break;
+        case spade: 
+        std::cout <<"spade";break;
+    }
+
+    string valueName;
+    if (val == 1) valueName = "Ace";
+    else if (val == 11) valueName = "Jack";
+    else if (val == 12) valueName = "Queen";
+    else if (val == 13) valueName = "King";
+    else valueName = std::to_string(val);
+
+    cout<<val<< " of " <<col<<endl; 
 }
+
+
--- Codes_LAB5/Player.h
+++ Codes_LAB5/Player.h
@@ -2,10 +2,45 @@ class Player {
 public:
     Player(CardsSet& cardPacket, bool isComputer) : packet(cardPacket), computer
     (isComputer) {}
-    int play();
+    int play(Card card);
 private:
     CardsSet inHand;
     CardsSet& packet;
     const bool computer;
     int countPoints();
 };
+
+int Player:: play(Card card){
+    //im not sure about the packet.take()
+    //because dont we already take it in the main
+
+    int points =0;
+    inHand.put(card);
+    cout<<"You get a card: ";
+    card.write();
+    points=countPoints();
+    cout<<"Your score is: "<< points << endl;
+    return points;
+
+
+
+}
+
+int Player:: countPoints(){
+    int aces = 0;
+    int total = 0;
+    for(int i = 0; i < inHand.numCards(); i++){
+        if(inHand.lookIn(i).value() ==1 && total<21){
+            aces++;
+            total+=14;
+        }
+        else{
+        total += inHand.lookIn(i).value();
+        }
+    }
+    if(total>21 && aces>0){
+        total -= (aces*13);
+    }
+    return total;
+
+}
